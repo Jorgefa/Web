f412cc432cc36789ce36103bb0a23d9471246d59
jorge <jfa0088@gmail.com>
2025-05-23 12:35:58 +0200

    deployed

--- src/context/TranslationContext.tsx
+++ src/context/TranslationContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+import React, { createContext, useContext, useState, ReactNode } from 'react';
 
 // Define available languages
 export type Language = 'es' | 'en';
